2a93f8ff61a8e82b6b4dd3f21b28f1a15fcf7e53
Decker Ayers <ayersdecker@gmail.com>
2024-08-05 21:24:00 -0400

    Project Started

---
+++ Terminal-01/Terminal-01/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Terminal_01
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Terminal-01/Terminal-01/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Terminal_01
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+
+}
